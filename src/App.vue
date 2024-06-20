0454d86afff88d111351a43cdb2f9b1ce262d588
RichtersHotline <KeenanTimmieWeb@gmail.com>
2024-06-20 11:49:57 +0200

    added salary calculation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1>Calculate your Salary</h1>
+<form class="container">
+<input type="number" id="workrate" placeholder="enter work rate" v-model="rate">
+<input type="number" id="workhours" placeholder="enter hours" v-model="workhrs ">
+<label id="salary"></label>
+<button @click.prevent="calculateSalary">Calculate Salary</button>
+<button @click.prevent="Clearer">Clear</button>
 
+
+
+
+
+</form>
+  
+
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+
+data() {
+ return {
+ aster:true,
+ rate: "",
+ workhrs: ""
+ }
+
+ },
+ computed: {
+
+
+ },
+
+ methods: {
+
+
+  toggleAsterisk() {
+   this.aster = !this.aster
+ 
+
+   
+   
+  },
+
+
+  calculateSalary() {
+
+ document.getElementById("salary").innerHTML = "R" + this.rate * this.workhrs
+
+
+  },
+  Clearer() {
+
+    this.rate = ""
+    this.workhrs = ""
+    document.getElementById("salary").innerHTML = ""
   }
+
+ }
+
 }
 </script>
 
@@ -22,5 +73,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.container { 
+
+  display:flex;
+  flex-direction:column;
+justify-content:center;
+width:20%;
+margin:auto;
+text-align:center;
+gap:5px;
 }
 </style>
